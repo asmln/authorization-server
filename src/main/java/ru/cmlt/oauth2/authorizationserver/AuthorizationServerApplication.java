0ca75285a2505477f7be45707801be6a09102d76
asmln <a.smln@yandex.ru>
2018-04-30 22:31:56 +0300

    oauth2 app: facebook, github, authserver

---
+++ src/main/java/ru/cmlt/oauth2/authorizationserver/AuthorizationServerApplication.java
@@ -0,0 +1,14 @@
+package ru.cmlt.oauth2.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+}
